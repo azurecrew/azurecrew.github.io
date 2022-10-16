850e0d20642ae53763592327624e23be6d257dc1
Jon Zlotnik <me@jonzlotnik.info>
2022-10-16 14:25:49 -0400

    setup tailwindcss for hugo site

---
+++ README.md
@@ -0,0 +1,10 @@
+# Azure Crew Website
+
+## Devlopment
+
+### Get going locally
+From root of repo:
+```bash
+npm install
+npm run start
+```
---
+++ postcss.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    plugins: {
+      "postcss-import": {
+        path: ['assets/css']
+      },
+      'tailwindcss/nesting': {},
+      tailwindcss: {},
+      autoprefixer: {},
+    }
+  }
+  
